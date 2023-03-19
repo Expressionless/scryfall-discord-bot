99e5889e97e54323e067bf7e8a72d6e47a43881a
Expressionless <rhysmchaen@gmail.com>
2023-03-19 18:31:33 +1100

    build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 ./build.sh
 
-docker  build --no-cache -f src/main/docker/Dockerfile.jvm  -t  slyio/dateq:latest .
+docker  build --no-cache -f src/main/docker/Dockerfile.jvm  -t  expressionless/scryfallq:latest .
 
 #clean up
 image_ids=$(docker images | grep dateq | grep none)
 if [ "${image_ids:-0}" == 0 ]; then
   echo 'Skip clean up'
 else
-  docker images | grep dateq | grep none | awk '{print $3}' | xargs docker rmi
+  docker images | grep scryfallq | grep none | awk '{print $3}' | xargs docker rmi
 fi
-
-docker tag slyio/dateq:latest slyio/dateq:latest
